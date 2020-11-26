48caa7e4d802e2908885429cddbde295203137c1
Matt Rixman <github@matt.rixman.org>
2020-11-25 17:55:16 -0700

    a redis test

---
+++ redis/pipeline.py
@@ -0,0 +1,16 @@
+import conducto as co
+
+img = co.Image(dockerfile="./Dockerfile", copy_repo=True)
+
+
+def main() -> co.Serial:
+    with co.Serial() as node:
+        node["ping"] = co.Exec(
+            "redis-cli -h redis-15233.c61.us-east-1-3.ec2.cloud.redislabs.com -p 15233 -a nO4bpNHpUne4PRearIOZrHYgU5N3wWsJ ping | grep PONG",
+            image=img,
+        )
+    return node
+
+
+if __name__ == "__main__":
+    co.main(default=main)
